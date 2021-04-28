a48dd7097b5251cc389d34eb72a954b0798cabdf
David Amorim Xavier (VSCODE) <webxddesign@hotmail.com>
2021-04-28 19:26:54 -0300

    delete

--- wp-content/cache/object/3ad/e08/3ade086c7154266a6e34e5518a1ef7da.php
+++
@@ -1,6 +0,0 @@
-ö×‰`<?php exit; ?>a:1:{s:7:"content";O:8:"stdClass":24:{s:2:"ID";i:35;s:11:"post_author";s:1:"1";s:9:"post_date";s:19:"2020-10-21 15:29:30";s:13:"post_date_gmt";s:19:"2020-10-21 15:29:30";s:12:"post_content";s:307:"<h1>Contact</h1>		
-					491.332.111
-					office@example.org
-					55th East St., New York
-			<h2>Get in touch and let's discuss your project</h2>		
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>(insert contact form here)";s:10:"post_title";s:7:"Contact";s:12:"post_excerpt";s:0:"";s:11:"post_status";s:7:"publish";s:14:"comment_status";s:6:"closed";s:11:"ping_status";s:6:"closed";s:13:"post_password";s:0:"";s:9:"post_name";s:7:"contact";s:7:"to_ping";s:0:"";s:6:"pinged";s:0:"";s:13:"post_modified";s:19:"2021-04-23 01:14:47";s:17:"post_modified_gmt";s:19:"2021-04-23 04:14:47";s:21:"post_content_filtered";s:0:"";s:11:"post_parent";i:0;s:4:"guid";s:48:"https://demo.elfwp.com/izo-agency-2/?page_id=605";s:10:"menu_order";i:0;s:9:"post_type";s:4:"page";s:14:"post_mime_type";s:0:"";s:13:"comment_count";s:1:"0";s:6:"filter";s:3:"raw";}}
